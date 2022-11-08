dc40066d6d30910cc848c2116308e1c7aa9efb66
jalal alirezaei <jalalpoker1@gamil.com>
2022-11-08 19:23:24 +0330

    first commit

---
+++ src/Uploader.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Jalal\Uploader;
+
+class Uploader {
+
+    private $path;
+
+    private $kind = 'image';
+
+    private $hash = false;
+
+
+    public function __construct() {
+
+    }
+
+
+    public function upload() {
+
+    }
+
+
+    protected function setPath() {
+
+    }
+
+
+}
